386839001e5331fb4475cac5c68c5718f12d71f4
Tzikas <niko.tzikas@gmail.com>
2020-05-11 17:33:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express =  require('express')
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  
+
+
+  socket.on('chat message to server', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message to client', msg);
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+  });
+
+
+});
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
